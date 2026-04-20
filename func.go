7e5ea937daf4dceeac57b2a53af907a89336afcb
zheng peng <pengzheng@huishoubao.com.cn>
2026-04-20 17:18:56 +0800

    add DrainBody

--- func.go
+++ func.go
@@ -1,6 +1,10 @@
 package httptool
 
-import "io"
+import (
+	"bytes"
+	"io"
+	"net/http"
+)
 
 func ReadAll(body io.Reader) (b []byte, err error) {
 	if body == nil {
@@ -14,4 +18,21 @@ func ReadAll(body io.Reader) (b []byte, err error) {
 		return nil, err
 	}
 	return b, nil
+}
+
+// DrainBody copy from net/http/httputil/dump.go
+func DrainBody(b io.ReadCloser) (r1 *bytes.Buffer, r2 io.ReadCloser, err error) {
+	var buf bytes.Buffer
+	if b == nil || b == http.NoBody {
+		// No copying needed. Preserve the magic sentinel meaning of NoBody.
+		return &buf, http.NoBody, nil
+	}
+
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, err
+	}
+	if err = b.Close(); err != nil {
+		return nil, b, err
+	}
+	return &buf, io.NopCloser(bytes.NewReader(buf.Bytes())), nil
 }
